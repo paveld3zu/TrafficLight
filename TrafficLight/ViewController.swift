321618ffab69e60ae9bbc33e9e5e1c8494163280
paveld3zu <pavel.karunny@yandex.ru>
2023-03-17 22:41:09 +0300

    Setup constraints

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var redCircleView: UIView!
+    @IBOutlet var yellowCircleView: UIView!
+    @IBOutlet var greenCircleView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redCircleView.layer.cornerRadius = 70
+        yellowCircleView.layer.cornerRadius = 70
+        greenCircleView.layer.cornerRadius = 70
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redCircleView.alpha == yellowCircleView.alpha {
+            redCircleView.alpha = 1
+            greenCircleView.alpha = 0.3
+        } else if yellowCircleView.alpha == greenCircleView.alpha {
+            yellowCircleView.alpha = 1
+            redCircleView.alpha = 0.3
+        } else if greenCircleView.alpha == redCircleView.alpha {
+            greenCircleView.alpha = 1
+            yellowCircleView.alpha = 0.3
+        }
+    }
 }
 
