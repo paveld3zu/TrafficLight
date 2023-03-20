61cfbbe88e66cd4e674df9ac30535e0f7f668a0a
paveld3zu <pavel.karunny@yandex.ru>
2023-03-20 22:58:19 +0300

    Final build

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,26 +15,34 @@ final class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
+        startButton.layer.cornerRadius = 15
+    }
+    override func viewWillLayoutSubviews() {
         redCircleView.layer.cornerRadius = redCircleView.frame.height / 2
         yellowCircleView.layer.cornerRadius = yellowCircleView.frame.height / 2
         greenCircleView.layer.cornerRadius = greenCircleView.frame.height / 2
-        startButton.layer.cornerRadius = 15
     }
     
     @IBAction func startButtonDidTapped() {
-        startButton.setTitle("NEXT", for: .normal)
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+    }
         
         if redCircleView.alpha == yellowCircleView.alpha {
-            redCircleView.alpha = 1
-            greenCircleView.alpha = 0.3
+            redCircleView.alpha = lightIsOn
+            greenCircleView.alpha = lightIsOff
         } else if yellowCircleView.alpha == greenCircleView.alpha {
-            yellowCircleView.alpha = 1
-            redCircleView.alpha = 0.3
+            yellowCircleView.alpha = lightIsOn
+            redCircleView.alpha = lightIsOff
         } else if greenCircleView.alpha == redCircleView.alpha {
-            greenCircleView.alpha = 1
-            yellowCircleView.alpha = 0.3
+            greenCircleView.alpha = lightIsOn
+            yellowCircleView.alpha = lightIsOff
         }
     }
 }
